57e2ccab4fcb297fee0c0bce8fa9991dc8a94303
Boban Acimovic <boban.acimovic@gmail.com>
2019-04-24 22:22:38 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/", handler(client))
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func handler(c *kubernetes.Clientset) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		list, err := c.CoreV1().Pods("").List(metav1.ListOptions{})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		for _, p := range list.Items {
+			w.Write([]byte("Pod: " + p.ObjectMeta.Name + "<br>"))
+			for _, cs := range p.Spec.Containers {
+				w.Write([]byte("Container: " + cs.Name + " Image: " + cs.Image + "<br>"))
+			}
+			for _, ics := range p.Spec.InitContainers {
+				w.Write([]byte("Init container: " + ics.Name + " Image: " + ics.Image + "<br>"))
+			}
+		}
+	})
+}
